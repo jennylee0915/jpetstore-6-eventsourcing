62f17e060567de6004b84bbe7e2e786a249a21d8
JennyLee <lee@lijiazhendeMacBook-Pro.local>
2024-01-08 02:51:57 +0800

    HSQLDB change to MySQL

--- src/main/java/org/mybatis/jpetstore/test/TestProductRepo.java
+++ src/main/java/org/mybatis/jpetstore/test/TestProductRepo.java
@@ -1,19 +1,33 @@
+/*
+ *    Copyright 2010-2024 the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       https://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
 package org.mybatis.jpetstore.test;
 
-import org.mybatis.jpetstore.domain.Category;
 import org.mybatis.jpetstore.domain.Product;
 
 public class TestProductRepo {
-    public static void main(String[] args) {
-        Product product = new Product();
+  public static void main(String[] args) {
+    Product product = new Product();
 
-        product.setProductId("EST-10000");
-        product.setName("Fish");
-        product.setDescription("Big Fish");
+    product.setProductId("EST-10000");
+    product.setName("Fish");
+    product.setDescription("Big Fish");
 
-        System.out.println(product);
-        product.getEvents().forEach(event -> {
-            System.out.println("Event: " + event);
-        });
-    }
+    System.out.println(product);
+    product.getEvents().forEach(event -> {
+      System.out.println("Event: " + event);
+    });
+  }
 }
